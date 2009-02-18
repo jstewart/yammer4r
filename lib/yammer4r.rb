9abc6c7000b2199cf3139c8fd5a07e8f1668d692
Jason Stewart <jstewart@fusionary.com>
2009-02-19 04:22:28 +0800

    Created gemspec
    Signed-off-by: Jim Patterson <jimp79@gmail.com>

---
+++ lib/yammer4r.rb
@@ -0,0 +1,13 @@
+require('rubygems')
+require('date')
+require('yaml')
+require('open-uri')
+require('json')
+require('oauth')
+
+$:.unshift(File.dirname(__FILE__))
+require('ext/core_ext')
+require('yammer/client')
+require('yammer/message')
+require('yammer/message_list')
+require('yammer/user')
